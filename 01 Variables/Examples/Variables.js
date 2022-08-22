37569e0f3eaab588b98d787be6f97cdb53e160d3
Shubham S Jagtap <63872951+ShubhamJagtap2000@users.noreply.github.com>
2022-08-22 12:36:08 +0530

    Create Variables.js

---
+++ 01 Variables/Examples/Variables.js
@@ -0,0 +1,10 @@
+//Types of variables in JavaScript
+
+//Can be re-assigned
+var variableOne = 'Linus Torvalds';
+
+//Can be updated & re-declared
+let variableTwo = 50;
+
+//Will never change
+const variableThree = 'Creator of the Linux Kernel';
