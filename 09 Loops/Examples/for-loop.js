058bf7b53742c43e3309ac3330e0b245d97542f4
Shubham S Jagtap <63872951+ShubhamJagtap2000@users.noreply.github.com>
2022-08-22 22:56:38 +0530

    Create for-loop.js

---
+++ 09 Loops/Examples/for-loop.js
@@ -0,0 +1,5 @@
+for (a = 1; a <= 10; a++) {
+    console.log(`Number: ${a}`); // Outputs 1-10 in our console
+}
+
+//We can have more than one value within our first statement, but they must be separated by commas: for (a = 1, b = 2; component-2; component-3) {}
