e330f03a4fc6a1bbb142392368046e9e0ff906bd
Shubham S Jagtap <63872951+ShubhamJagtap2000@users.noreply.github.com>
2022-08-22 19:48:37 +0530

    Create switch-case.js

---
+++ 05 Switch Cases/Examples/switch-case.js
@@ -0,0 +1,27 @@
+const animal = 3;
+
+switch (animal) {
+
+case 1:
+
+document.write('Cow');
+
+break;
+
+case 2:
+
+document.write('Chicken');
+
+break;
+
+case 3:
+
+document.write('Monkey');
+
+break;
+
+default:
+
+document.write('Animal?');
+
+} // Outputs Monkey
